1dbf751bd000c7f57afeb8cc26d5d21131760c5c
aster-anto <aster.anto@gmail.com>
2023-09-22 01:15:49 +0100

    initial commit

---
+++ src/test/java/com/springboard/hackathon/wraparound/WrapAroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboard.hackathon.wraparound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapAroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
